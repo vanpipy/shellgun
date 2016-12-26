4a4b31f3b9b5e0b2c6842fe4b4f4acf6df895ee3
vanpipy <vanpipy@gmail.com>
2016-12-26 11:02:45 +0800

    Fix create md file as no sync.

--- bin/gun-createmd.js
+++ bin/gun-createmd.js
@@ -9,16 +9,26 @@ const path = require('path');
 //void
 function main () {
     const nowday = new Date();
-    const today = `${process.cwd()}/${nowday.getFullYear()}${nowday.getMonth() + 1}${nowday.getDate()}.md`;
-    const fd = fs.openSync(today, 'w', (err) => {
+    const currentPath = process.cwd();
+    const today = `${nowday.getFullYear()}${nowday.getMonth() + 1}${nowday.getDate()}`;
+
+    fs.open(`${currentPath}/${today}.md`, 'w', (err, fd) => {
         if (err) {
             console.log(`Error: ${err}`);
             return;
         }
-    });
 
-    fs.writeSync(fd);
-    fs.closeSync(fd);
+        const buffer = new Buffer(`# ${today}\n`);
+
+        fs.write(fd, buffer, 0, buffer.length, 0, (err, written, buffer) => {
+            if (err) {
+                console.log(`Error: ${err}`);
+                return;
+            }
+        });
+
+        fs.close(fd);
+    });
 }
 
 main();
